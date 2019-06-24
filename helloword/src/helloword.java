032fc5356ac2a38853138a2c1d62f37952c3f32d
ggp <535464431@qq.com>
2019-06-24 20:17:15 +0800

    初始化

---
+++ helloword/src/helloword.java
@@ -0,0 +1,12 @@
+/**
+ * @author ggp
+ * @version V1.0
+ * @Package PACKAGE_NAME
+ * @date 2019/6/24 20:15
+ * @Copyright © 2019-2020
+ */
+public class helloword {
+    public static void main(String[] args) {
+        System.out.println("1111");
+    }
+}
